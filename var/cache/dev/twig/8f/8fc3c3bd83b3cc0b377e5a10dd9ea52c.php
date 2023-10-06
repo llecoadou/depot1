55823345238464ca7a911ca58b9cb719661e91ad
kyemadje <kenael.yemadje@hotmail.com>
2023-10-06 10:00:07 +0200

    navbar

---
+++ var/cache/dev/twig/8f/8fc3c3bd83b3cc0b377e5a10dd9ea52c.php
@@ -0,0 +1,300 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* base.html.twig */
+class __TwigTemplate_daa0b547cd44608b66bd290b3ac8dac3 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'stylesheets' => [$this, 'block_stylesheets'],
+            'javascripts' => [$this, 'block_javascripts'],
+            'header' => [$this, 'block_header'],
+            'main' => [$this, 'block_main'],
+            'footer' => [$this, 'block_footer'],
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "base.html.twig"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "base.html.twig"));
+
+        // line 1
+        echo "<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=\"UTF-8\">
+        <title>";
+        // line 5
+        $this->displayBlock('title', $context, $blocks);
+        echo "</title>
+        <link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%2296%22></text></svg>\">
+        ";
+        // line 8
+        echo "        <meta charset=\"utf-8\" />
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1,shrink-to-fit=no\" />
+        <title>KAL</title>
+
+        ";
+        // line 12
+        $this->displayBlock('stylesheets', $context, $blocks);
+        // line 18
+        echo "        ";
+        $this->displayBlock('javascripts', $context, $blocks);
+        // line 25
+        echo "    </head>
+    <body>
+        <div class=\"container-fluid\" id=\"top\">
+        ";
+        // line 28
+        $this->displayBlock('header', $context, $blocks);
+        // line 31
+        echo "        <div id=\"main\" class=\"row\" >
+        ";
+        // line 32
+        $this->displayBlock('main', $context, $blocks);
+        // line 34
+        echo "        </div>
+        <div id=\"message\"></div>
+        ";
+        // line 36
+        $this->displayBlock('footer', $context, $blocks);
+        // line 39
+        echo "        </div>
+    </body>
+    
+</html>";
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+    }
+
+    // line 5
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        echo "Welcome!";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    // line 12
+    public function block_stylesheets($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        // line 13
+        echo "            ";
+        echo twig_escape_filter($this->env, $this->env->getFunction('encore_entry_link_tags')->getCallable()("app"), "html", null, true);
+        echo "
+            <link rel=\"stylesheet\"href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\"/>
+            <link rel=\"stylesheet\" href=\"";
+        // line 15
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("asset/css/style.css"), "html", null, true);
+        echo "\"/>
+            <link rel=\"stylesheet\"href=\"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css\">
+        ";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    // line 18
+    public function block_javascripts($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
+
+        // line 19
+        echo "            ";
+        echo twig_escape_filter($this->env, $this->env->getFunction('encore_entry_script_tags')->getCallable()("app"), "html", null, true);
+        echo "
+            <script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\"></script>
+            <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\"></script>
+            <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\"></script>
+            <script src=\"";
+        // line 23
+        echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("asset/js/script.js"), "html", null, true);
+        echo "\"></script>
+        ";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    // line 28
+    public function block_header($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "header"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "header"));
+
+        // line 29
+        echo "            ";
+        $this->loadTemplate("partials/__header.html.twig", "base.html.twig", 29)->display($context);
+        // line 30
+        echo "        ";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    // line 32
+    public function block_main($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "main"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "main"));
+
+        // line 33
+        echo "        ";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    // line 36
+    public function block_footer($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "footer"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "footer"));
+
+        // line 37
+        echo "            ";
+        $this->loadTemplate("partials/__footer.html.twig", "base.html.twig", 37)->display($context);
+        // line 38
+        echo "        ";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "base.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  236 => 38,  233 => 37,  223 => 36,  213 => 33,  203 => 32,  193 => 30,  190 => 29,  180 => 28,  168 => 23,  160 => 19,  150 => 18,  137 => 15,  131 => 13,  121 => 12,  102 => 5,  89 => 39,  87 => 36,  83 => 34,  81 => 32,  78 => 31,  76 => 28,  71 => 25,  68 => 18,  66 => 12,  60 => 8,  55 => 5,  49 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset=\"UTF-8\">
+        <title>{% block title %}Welcome!{% endblock %}</title>
+        <link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%2296%22></text></svg>\">
+        {# Run `composer require symfony/webpack-encore-bundle` to start using Symfony UX #}
+        <meta charset=\"utf-8\" />
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1,shrink-to-fit=no\" />
+        <title>KAL</title>
+
+        {% block stylesheets %}
+            {{ encore_entry_link_tags('app') }}
+            <link rel=\"stylesheet\"href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\"/>
+            <link rel=\"stylesheet\" href=\"{{ asset('asset/css/style.css') }}\"/>
+            <link rel=\"stylesheet\"href=\"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css\">
+        {% endblock %}
+        {% block javascripts %}
+            {{ encore_entry_script_tags('app') }}
+            <script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\"></script>
+            <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\"></script>
+            <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\"></script>
+            <script src=\"{{ asset('asset/js/script.js') }}\"></script>
+        {% endblock %}
+    </head>
+    <body>
+        <div class=\"container-fluid\" id=\"top\">
+        {% block header %}
+            {% include \"partials/__header.html.twig\" %}
+        {% endblock %}
+        <div id=\"main\" class=\"row\" >
+        {% block main %}
+        {% endblock %}
+        </div>
+        <div id=\"message\"></div>
+        {% block footer %}
+            {% include \"partials/__footer.html.twig\" %}
+        {% endblock %}
+        </div>
+    </body>
+    
+</html>", "base.html.twig", "C:\\wamp64\\www\\Ap\\Projet\\depot1\\templates\\base.html.twig");
+    }
+}
---
+++ var/cache/dev/twig/b8/b865fb983ef4764e0b85c97719e84118.php
@@ -0,0 +1,146 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* partials/__header.html.twig */
+class __TwigTemplate_9d579cf2073e4a6dcf7145e620040e68 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "partials/__header.html.twig"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "partials/__header.html.twig"));
+
+        // line 1
+        echo "<!-- ======= Header ======= -->
+  <header id=\"header\" class=\"d-flex align-items-center bg-dark\">
+    <div class=\"container d-flex justify-content-between \">
+
+      <div id=\"logo\">
+        <h1><a href=\"index.html\" style=\"color: #2F47A0;\">K<span style=\"color:#CA9165;\">AL</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href=\"index.html\"><img src=\"assets/img/logo.png\" alt=\"\"></a>-->
+      </div>
+
+      <nav id=\"navbar\" class=\"navbar\">
+        <ul>
+          <li><a class=\"nav-link scrollto active\" href=\"#hero\">Accueil</a></li>
+          <li><a class=\"nav-link scrollto\" href=\"#about\">A propos</a></li>
+          <li><a class=\"nav-link scrollto\" href=\"#services\">Services</a></li>
+          <li><a class=\"nav-link scrollto \" href=\"#portfolio\">Portfolio</a></li>
+          <li><a class=\"nav-link scrollto\" href=\"#team\">Team</a></li>
+          <li class=\"dropdown\"><a href=\"#\"><span>Drop Down</span> <i class=\"bi bi-chevron-down\"></i></a>
+            <ul>
+              <li><a href=\"#\">Drop Down 1</a></li>
+              <li class=\"dropdown\"><a href=\"#\"><span>Deep Drop Down</span> <i class=\"bi bi-chevron-right\"></i></a>
+                <ul>
+                  <li><a href=\"#\">Deep Drop Down 1</a></li>
+                  <li><a href=\"#\">Deep Drop Down 2</a></li>
+                  <li><a href=\"#\">Deep Drop Down 3</a></li>
+                  <li><a href=\"#\">Deep Drop Down 4</a></li>
+                  <li><a href=\"#\">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href=\"#\">Drop Down 2</a></li>
+              <li><a href=\"#\">Drop Down 3</a></li>
+              <li><a href=\"#\">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a class=\"nav-link scrollto\" href=\"#contact\">Contact</a></li>
+        </ul>
+        <i class=\"bi bi-list mobile-nav-toggle\"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->";
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "partials/__header.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<!-- ======= Header ======= -->
+  <header id=\"header\" class=\"d-flex align-items-center bg-dark\">
+    <div class=\"container d-flex justify-content-between \">
+
+      <div id=\"logo\">
+        <h1><a href=\"index.html\" style=\"color: #2F47A0;\">K<span style=\"color:#CA9165;\">AL</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href=\"index.html\"><img src=\"assets/img/logo.png\" alt=\"\"></a>-->
+      </div>
+
+      <nav id=\"navbar\" class=\"navbar\">
+        <ul>
+          <li><a class=\"nav-link scrollto active\" href=\"#hero\">Accueil</a></li>
+          <li><a class=\"nav-link scrollto\" href=\"#about\">A propos</a></li>
+          <li><a class=\"nav-link scrollto\" href=\"#services\">Services</a></li>
+          <li><a class=\"nav-link scrollto \" href=\"#portfolio\">Portfolio</a></li>
+          <li><a class=\"nav-link scrollto\" href=\"#team\">Team</a></li>
+          <li class=\"dropdown\"><a href=\"#\"><span>Drop Down</span> <i class=\"bi bi-chevron-down\"></i></a>
+            <ul>
+              <li><a href=\"#\">Drop Down 1</a></li>
+              <li class=\"dropdown\"><a href=\"#\"><span>Deep Drop Down</span> <i class=\"bi bi-chevron-right\"></i></a>
+                <ul>
+                  <li><a href=\"#\">Deep Drop Down 1</a></li>
+                  <li><a href=\"#\">Deep Drop Down 2</a></li>
+                  <li><a href=\"#\">Deep Drop Down 3</a></li>
+                  <li><a href=\"#\">Deep Drop Down 4</a></li>
+                  <li><a href=\"#\">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href=\"#\">Drop Down 2</a></li>
+              <li><a href=\"#\">Drop Down 3</a></li>
+              <li><a href=\"#\">Drop Down 4</a></li>
+            </ul>
+          </li>
+          <li><a class=\"nav-link scrollto\" href=\"#contact\">Contact</a></li>
+        </ul>
+        <i class=\"bi bi-list mobile-nav-toggle\"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->", "partials/__header.html.twig", "C:\\wamp64\\www\\Ap\\Projet\\depot1\\templates\\partials\\__header.html.twig");
+    }
+}
